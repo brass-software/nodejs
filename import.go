10b46a312dc75f5fe08997374bbf6df484b01d91
Mike Rybka <merybka@gmail.com>
2024-03-06 08:58:05 -0500

    Add String()

--- import.go
+++ import.go
@@ -1,7 +1,22 @@
 package nodejs
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Import struct {
-	From         string
-	DefaultNames string
-	Names        []string
+	From    string
+	Default string
+	Named   []string
+}
+
+func (i *Import) String() string {
+	if len(i.Named) == 0 {
+		return fmt.Sprintf("import %s from \"%s\";\n", i.Default, i.From)
+	}
+	if i.Default == "" {
+		return fmt.Sprintf("import { %s } from \"%s\";\n", strings.Join(i.Named, ", "), i.From)
+	}
+	return fmt.Sprintf("import %s, { %s } from \"%s\";\n", i.Default, strings.Join(i.Named, ", "), i.From)
 }
